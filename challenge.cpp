e0dd8c2a5758d74ee8336414ea03a7c294348d37
GiovanePS <giovanespace@gmail.com>
2023-09-27 06:03:07 +0000

    pequeno prototipo

--- challenge.cpp
+++ challenge.cpp
@@ -1,10 +1,23 @@
-#include <stdio.h>
-#include <libcryptosec/MessageDigest.h>
+#include <iostream>
+#include <vector>
+#include <fstream>
 
 int main(int argc, char **argv) {
-	printf("Hello There!\n");
 	
-	MessageDigest::loadMessageDigestAlgorithms();
-	
+	std::ifstream pdfFile(argv[1]);
+	int operadores;
+	std::vector<std::string> chaves_adicionadas;
+	std::string key;
+
+	std::cout << "Quantos operadores?";
+	std::cin >> operadores;
+	for (int i = 0; i < operadores; i++) {
+		std::cout << "Digite a chave do " << i << "º operador: ";
+		std::cin >> key;
+		chaves_adicionadas.push_back(key);
+	}
+
+	//Função para assinar o pdfFile com cada key de chaves_adicionadas.
+
 	return 0;
 }
