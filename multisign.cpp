ca4c50a9d7792f157081541982419e3c8204a3f1
Bridge-PC <giovanespace@gmail.com>
2023-09-28 19:40:56 +0000

    chave simétrica adicionada

--- multisign.cpp
+++ multisign.cpp
@@ -1,38 +1,27 @@
 #include <iostream>
-#include <vector>
 #include <fstream>
-#include <RSAKeyPair.h>
-#include <Signer.h>
-#include <AsymmetricKey.h>
+#include <SymmetricKey.h>
+#include <ByteArray.h>
 
 int main(int argc, char* argv[]) {
 	
-	if (argc == 1) {
-		std::cout << "Bad Usage!" << '\n';
-		std::cout << "Argumentos faltando!" << std::endl;
+	if (argc < 2 || argc > 5) {
+		cout << "Bad usage!" << '\n';
+		cout << "Leia a documentação" << endl;
 		return 1;
 	}
 
+	ByteArray base64Key = std::string("+QKrok03Mo5eg2QqKvK/WJO7AiiUADW08QbL558AD2E="); // Chave simétrica de 32 bits para todo binário mltsgn.
+	SymmetricKey symmetricKey = SymmetricKey(base64Key, SymmetricKey::AES_128);
+
+
 	std::string argv2 = argv[2];
-	if (argv2 == "-s" || argv2 == "--signatures") {}
-
-	// std::ifstream pdfFile(argv[1]);
-	std::ofstream outputFile("mlt_keys");
-	int operadores;
-	std::vector<KeyPair> chaves_adicionadas;
-	std::string key;
-
-	std::cout << "Quantos operadores: ";
-	std::cin >> operadores;
-
-	for (int i = 0; i < operadores; i++) {
-		chaves_adicionadas.push_back(RSAKeyPair(2048));
-		std::cout << chaves_adicionadas[i].getPemEncoded();
+	if (argv2 == "-s" || argv2 == "--signatures") {
+		// Parte de iterar sobre arquivos para guardar as chaves privadas de dentro de uma pasta.
 	}
 
-
-
-	//Função para assinar o pdfFile com cada key de chaves_adicionadas.
+	std::ifstream pdfFile(argv[1]);
+	std::ofstream outputFile("mlt_keys");
 
 	return 0;
 }
--- to_sign.cpp
+++ to_sign.cpp
@@ -1,21 +1,43 @@
 #include <iostream>
+#include <fstream>
+// #include <Signer.h>
+
+using std::cout;
+using std::endl;
 
 int main(int argc, char* argv[]) {
 
+    if (argc > 3) {
+        cout << "Bad usage!" << '\n';
+        cout << "Leia a documentação" << std::endl;
+        return 1;
+    }
+
+    std::ifstream mlt_keys("mlt_keys", std::ios::in);
+
+	if (!mlt_keys) {
+		cout << "Arquivo mlt_keys não encontrado." << endl;
+		return 1;
+	}
+
     if (argc == 1) {
+        // Se algum operador já tiver assinado mas não completamente.
+        cout << "Faltam x assinaturas." << endl;
         // Se não tiver nenhuma assinatura:
-        std::cout << "Nenhuma assinatura coletada ainda." << std::endl;
+        cout << "Nenhuma assinatura coletada ainda." << endl;
+        //Se todos os operadores já tiverem assinado:
+        cout << "Todas as assinaturas foram coletadas!" << endl;
         return 0;
     }
     
     std::string argv1 = argv[1];
     if (argv1 == "-a" || argv1 == "--add") {
         // Para assinar documentos em cima do to_sign
-        std::cout << "Assinado com sucesso!" << std::endl;
+        cout << "Assinado com sucesso!" << endl;
     } else {
         // Se der erro na hora de executar.
-        std::cout << "Bad usage!" << '\n';
-        std::cout << "Leia a documentação";
+        cout << "Bad usage!" << '\n';
+        cout << "Leia a documentação";
     }
 
     return 0;
