1304fed6d9fe41e607216b7e4c684dfb71176371
GiovanePS <giovanespace@gmail.com>
2023-09-29 03:36:56 +0000

    aplicando os primeiros recursos de multisign

--- multisign.cpp
+++ multisign.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
 #include <fstream>
-#include <SymmetricKey.h>
 #include <ByteArray.h>
+#include <SymmetricKey.h>
+#include <SymmetricCipher.h>
 
 int main(int argc, char* argv[]) {
 	
-	if (argc < 2 || argc > 5) {
+	if (argc != 5) {
 		cout << "Bad usage!" << '\n';
 		cout << "Leia a documentação" << endl;
 		return 1;
 	}
 
+	// std::ifstream pdfFile(argv[4]);
+	std::ofstream outputFile("mlt_keys", std::ios::out);
 	ByteArray base64Key = std::string("+QKrok03Mo5eg2QqKvK/WJO7AiiUADW08QbL558AD2E="); // Chave simétrica de 32 bits para todo binário mltsgn.
 	SymmetricKey symmetricKey = SymmetricKey(base64Key, SymmetricKey::AES_128);
 
@@ -18,10 +21,10 @@ int main(int argc, char* argv[]) {
 	std::string argv2 = argv[2];
 	if (argv2 == "-s" || argv2 == "--signatures") {
 		// Parte de iterar sobre arquivos para guardar as chaves privadas de dentro de uma pasta.
+		for (int i = 0; i < 10; i++) {
+			outputFile << "Texto" << '\n';
+		}
 	}
-
-	std::ifstream pdfFile(argv[1]);
-	std::ofstream outputFile("mlt_keys");
 
 	return 0;
 }
