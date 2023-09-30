745da49f5d2a780443d404505774da801e49c07a
GiovanePS <giovanespace@gmail.com>
2023-09-30 06:33:23 +0000

    apenas salvando trabalho feito antes de dormir, mesmo estando bugado kk

--- src/multisign.cpp
+++ src/multisign.cpp
@@ -1,30 +1,51 @@
 #include <iostream>
 #include <fstream>
+#include <sys/types.h>
+#include <dirent.h>
 #include <ByteArray.h>
-#include <SymmetricKey.h>
-#include <SymmetricCipher.h>
+#include <KeyPair.h>
+
+using std::cout;
+using std::endl;
+using std::cerr;
+
+using std::string;
 
 int main(int argc, char* argv[]) {
 	
-	if (argc != 5) {
-		cout << "Bad usage!" << '\n';
-		cout << "Leia a documentação" << endl;
-		return 1;
-	}
-
-	// std::ifstream pdfFile(argv[4]);
+	// if (argc != 5) {
+	// 	cout << "Bad usage!" << '\n';
+	// 	cout << "Leia a documentação" << endl;
+	// 	return 1;
+	// }
 	std::ofstream outputFile("mlt_keys", std::ios::out);
-	ByteArray base64Key = std::string("+QKrok03Mo5eg2QqKvK/WJO7AiiUADW08QbL558AD2E="); // Chave simétrica de 32 bits para todo binário mltsgn.
-	SymmetricKey symmetricKey = SymmetricKey(base64Key, SymmetricKey::AES_128);
+	
 
-
-	std::string argv2 = argv[2];
-	if (argv2 == "-s" || argv2 == "--signatures") {
+	string bufferString;
+	string flag1 = argv[1];
+	if (flag1 == "-s" || flag1 == "--signatures") {
 		// Parte de iterar sobre arquivos para guardar as chaves privadas de dentro de uma pasta.
-		for (int i = 0; i < 10; i++) {
-			outputFile << "Texto" << '\n';
+		DIR* dir = opendir(argv[3]);
+		if (!dir) {
+			cerr << "Erro ao abrir pasta." << endl;
+			return 1;
+		}
+
+		struct dirent* entry;
+		while ((entry = readdir(dir))) {
+			std::cout << entry->d_name << std::endl;
 		}
 	}
+
+	// string flag2 = argv[3];
+	// if (flag2 == "-i" || flag2 == "--input") {
+	// 	// Parte de abrir arquivo PDF que irá ser assinado.
+	// 	std::ifstream pdfFile(argv[4]);
+	// }
+
+	// outputFile << bufferString;
+
+	// outputFile << output.toString();
 
 	return 0;
 }
